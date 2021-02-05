5ee6aa7fc794b573bf55972cc1dba1c0eecf3b0d
chenli <chenli@letto8.com>
2021-02-05 17:42:28 +0800

    测试代码提交

--- app/src/main/java/com/cl/myapplication/MainActivity.kt
+++ app/src/main/java/com/cl/myapplication/MainActivity.kt
@@ -1,15 +1,116 @@
 package com.cl.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.kongqw.serialportlibrary.Device
+import com.kongqw.serialportlibrary.SerialPortManager
+import com.kongqw.serialportlibrary.listener.OnOpenSerialPortListener
+import com.kongqw.serialportlibrary.listener.OnSerialPortDataListener
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnOpenSerialPortListener {
 
-
+    private val TAG = MainActivity::class.java.simpleName
+    val DEVICE = "device"
+    private var mSerialPortManager: SerialPortManager? = null
+    private var mToast: Toast? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val device = intent.getSerializableExtra(DEVICE) as Device?
+        Log.i(TAG, "onCreate: device = $device")
+        if (null == device) {
+            finish()
+            return
+        }
+        mSerialPortManager = SerialPortManager()
+
+        // 打开串口
+
+        // 打开串口
+        val openSerialPort = mSerialPortManager!!.setOnOpenSerialPortListener(this)
+            .setOnSerialPortDataListener(object : OnSerialPortDataListener {
+                override fun onDataReceived(bytes: ByteArray) {
+                    Log.i(
+                        TAG,
+                        "onDataReceived [ byte[] ]: " + Arrays.toString(bytes)
+                    )
+                    Log.i(
+                        TAG,
+                        "onDataReceived [ String ]: " + String(bytes)
+                    )
+                    runOnUiThread { showToast(String.format("接收\n%s", String(bytes))) }
+                }
+
+                override fun onDataSent(bytes: ByteArray) {
+                    Log.i(
+                        TAG,
+                        "onDataSent [ byte[] ]: " + Arrays.toString(bytes)
+                    )
+                    Log.i(
+                        TAG,
+                        "onDataSent [ String ]: " + String(bytes)
+                    )
+                    runOnUiThread { showToast(String.format("发送\n%s", String(bytes))) }
+                }
+            })
+            .openSerialPort(device.file, 115200)
+
+        Log.i(
+            TAG,
+            "onCreate: openSerialPort = $openSerialPort"
+        )
     }
+
+
+    override fun onSuccess(device: File?) {
+        Toast.makeText(
+            this,
+            String.format("串口 [%s] 打开成功", device!!.path),
+            Toast.LENGTH_SHORT
+        ).show()
+
+    }
+
+    override fun onFail(device: File?, status: OnOpenSerialPortListener.Status?) {
+
+    }
+
+    fun onSend(view: View) {
+        val editTextSendContent = et_send_content.text.toString()
+        if (TextUtils.isEmpty(editTextSendContent)) {
+            Log.i(TAG, "onSend: 发送内容为 null")
+            return
+        }
+        val sendContentBytes = editTextSendContent.toByteArray()
+        val sendBytes = mSerialPortManager!!.sendBytes(sendContentBytes)
+        Log.i(
+            TAG,
+            "onSend: sendBytes = $sendBytes"
+        )
+        showToast(if (sendBytes) "发送成功" else "发送失败")
+    }
+
+
+    /**
+     * Toast
+     *
+     * @param content content
+     */
+    private fun showToast(content: String) {
+        if (null == mToast) {
+            mToast = Toast.makeText(applicationContext, null, Toast.LENGTH_SHORT)
+        }
+        mToast?.setText(content)
+        mToast?.show()
+    }
+
 }
---
+++ app/src/main/java/com/cl/myapplication/SelectSerialPortActivity.kt
@@ -0,0 +1,43 @@
+package com.cl.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import androidx.appcompat.app.AppCompatActivity
+import com.cl.myapplication.adapter.DeviceAdapter
+import com.kongqw.serialportlibrary.SerialPortFinder
+import kotlinx.android.synthetic.main.activity_select_serial_port.*
+
+class SelectSerialPortActivity : AppCompatActivity(), OnItemClickListener {
+
+
+    private var mDeviceAdapter: DeviceAdapter? = null
+    val DEVICE = "device"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_serial_port)
+
+        val serialPortFinder = SerialPortFinder()
+
+        val devices =
+            serialPortFinder.devices
+
+        if (lv_devices != null) {
+            mDeviceAdapter = DeviceAdapter(applicationContext, devices)
+            lv_devices.adapter = mDeviceAdapter
+            lv_devices.onItemClickListener = this
+        }
+
+    }
+
+    override fun onItemClick(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+        val device = mDeviceAdapter!!.getItem(position)
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra(DEVICE, device)
+        startActivity(intent)
+    }
+}
