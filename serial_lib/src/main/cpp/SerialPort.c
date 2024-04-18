68b5b64305d6a44449240038914a24308e9368ab
陈力 <cl_666@yeah.net>
2024-04-18 18:46:29 +0800

    打开效验位相关代码

--- serial_lib/src/main/cpp/SerialPort.c
+++ serial_lib/src/main/cpp/SerialPort.c
@@ -1,3 +1,5 @@
+
+
 /*
  * Copyright 2009-2011 Cedric Priscal
  *
@@ -32,40 +34,40 @@ static const char *TAG="serial_port";
 
 static speed_t getBaudrate(jint baudrate)
 {
-	switch(baudrate) {
-	case 0: return B0;
-	case 50: return B50;
-	case 75: return B75;
-	case 110: return B110;
-	case 134: return B134;
-	case 150: return B150;
-	case 200: return B200;
-	case 300: return B300;
-	case 600: return B600;
-	case 1200: return B1200;
-	case 1800: return B1800;
-	case 2400: return B2400;
-	case 4800: return B4800;
-	case 9600: return B9600;
-	case 19200: return B19200;
-	case 38400: return B38400;
-	case 57600: return B57600;
-	case 115200: return B115200;
-	case 230400: return B230400;
-	case 460800: return B460800;
-	case 500000: return B500000;
-	case 576000: return B576000;
-	case 921600: return B921600;
-	case 1000000: return B1000000;
-	case 1152000: return B1152000;
-	case 1500000: return B1500000;
-	case 2000000: return B2000000;
-	case 2500000: return B2500000;
-	case 3000000: return B3000000;
-	case 3500000: return B3500000;
-	case 4000000: return B4000000;
-	default: return -1;
-	}
+    switch(baudrate) {
+        case 0: return B0;
+        case 50: return B50;
+        case 75: return B75;
+        case 110: return B110;
+        case 134: return B134;
+        case 150: return B150;
+        case 200: return B200;
+        case 300: return B300;
+        case 600: return B600;
+        case 1200: return B1200;
+        case 1800: return B1800;
+        case 2400: return B2400;
+        case 4800: return B4800;
+        case 9600: return B9600;
+        case 19200: return B19200;
+        case 38400: return B38400;
+        case 57600: return B57600;
+        case 115200: return B115200;
+        case 230400: return B230400;
+        case 460800: return B460800;
+        case 500000: return B500000;
+        case 576000: return B576000;
+        case 921600: return B921600;
+        case 1000000: return B1000000;
+        case 1152000: return B1152000;
+        case 1500000: return B1500000;
+        case 2000000: return B2000000;
+        case 2500000: return B2500000;
+        case 3000000: return B3000000;
+        case 3500000: return B3500000;
+        case 4000000: return B4000000;
+        default: return -1;
+    }
 }
 
 /*
@@ -151,23 +153,42 @@ JNIEXPORT jobject JNICALL Java_com_kongqw_serialportlibrary_SerialPort_open
                 return NULL;
         }
 
-        // Set parity
         switch (parity) {
-            case 0: // None
-                cfg.c_cflag &= ~PARENB;
+            case 0:
+                cfg.c_cflag &= ~PARENB;    //PARITY OFF
                 break;
-            case 1: // Odd
-                cfg.c_cflag |= PARENB;
-                cfg.c_cflag |= PARODD;
+            case 1:
+                cfg.c_cflag |= (PARODD | PARENB);   //PARITY ODD
+                cfg.c_iflag &= ~IGNPAR;
+                cfg.c_iflag |= PARMRK;
+                cfg.c_iflag |= INPCK;
                 break;
-            case 2: // Even
+            case 2:
+                cfg.c_iflag &= ~(IGNPAR | PARMRK); //PARITY EVEN
+                cfg.c_iflag |= INPCK;
                 cfg.c_cflag |= PARENB;
                 cfg.c_cflag &= ~PARODD;
                 break;
+            case 3:
+                //  PARITY SPACE
+                cfg.c_iflag &= ~IGNPAR;             //  Make sure wrong parity is not ignored
+                cfg.c_iflag |= PARMRK;              //  Marks parity error, parity error
+                //  is given as three char sequence
+                cfg.c_iflag |= INPCK;               //  Enable input parity checking
+                cfg.c_cflag |= PARENB | CMSPAR;     //  Enable parity and set space parity
+                cfg.c_cflag &= ~PARODD;             //
+                break;
+            case 4:
+                //  PARITY MARK
+                cfg.c_iflag &= ~IGNPAR;             //  Make sure wrong parity is not ignored
+                cfg.c_iflag |= PARMRK;              //  Marks parity error, parity error
+                //  is given as three char sequence
+                cfg.c_iflag |= INPCK;               //  Enable input parity checking
+                cfg.c_cflag |= PARENB | CMSPAR | PARODD;
+                break;
             default:
-                LOGE("Invalid parity");
-                close(fd);
-                return NULL;
+                cfg.c_cflag &= ~PARENB;
+                break;
         }
 
         // Set baud rate
